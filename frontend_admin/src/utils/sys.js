786cfe96a6a94cd13802a0fa1dfdc5775c74222f
晓飞 <1517996849@qq.com>
2026-06-10 13:33:33 +0800

    fix: 修复字典数据获取逻辑和工具栏显示条件
    1. 调整getSysDict函数获取数据的处理逻辑
    2. 移除工具栏显示的canManage权限判断
    吃了一包卫龙辣条

--- frontend_admin/src/utils/sys.js
+++ frontend_admin/src/utils/sys.js
@@ -4,7 +4,7 @@ export async function getSysDict(...sys) {
     const results = await Promise.all(sys.map(item =>
         get_getPublicItemsPaged({
             dictType: item
-        }).then(res => res.code === 200 ? res.data.data : [])
+        }).then(res => res.code === 200 ? res.data : [])
     ))
 
     return Object.fromEntries(sys.map((key, i) => [key, results[i]]))
